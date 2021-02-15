48341dbf875680d5a3864cc1e32beccb8a040842
zach sanders <zach.sanders@revature.net>
2021-02-15 15:12:53 -0500

    complete controller merge

--- aspnet/RVTR.Account.Service/Controllers/AccountController.cs
+++ aspnet/RVTR.Account.Service/Controllers/AccountController.cs
@@ -278,6 +278,246 @@ namespace RVTR.Account.Service.Controllers
       }
 
     }
+    /// <summary>
+    /// Deletes a user's payment information
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeletePayment(int id)
+    {
+      try
+      {
+        _logger.LogDebug("Deleting a payment by its ID number...");
 
+        await _unitOfWork.Payment.DeleteAsync(id);
+        await _unitOfWork.CommitAsync();
+
+        _logger.LogInformation($"Deleted the payment with ID number {id}.");
+
+        return Ok(MessageObject.Success);
+      }
+      catch
+      {
+        _logger.LogWarning($"Payment with ID number {id} does not exist.");
+
+        return NotFound(new ErrorObject($"Payment with ID number {id} does not exist"));
+      }
+
+    }
+
+    /// <summary>
+    /// Retrieves all payments
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<PaymentModel>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetPayment()
+    {
+      _logger.LogInformation($"Retrieved the payments.");
+
+      return Ok(await _unitOfWork.Payment.SelectAsync());
+    }
+
+    /// <summary>
+    /// Retrieves a payment by payment ID number
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(PaymentModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetPayment(int id)
+    {
+      PaymentModel paymentModel;
+
+      _logger.LogDebug("Getting a payment by its ID number...");
+
+      paymentModel = await _unitOfWork.Payment.SelectAsync(id);
+
+
+      if (paymentModel is PaymentModel thePayment)
+      {
+        _logger.LogInformation($"Retrieved the payment with ID: {id}.");
+
+        return Ok(thePayment);
+      }
+
+      _logger.LogWarning($"Payment with ID number {id} does not exist.");
+
+      return NotFound(new ErrorObject($"Payment with ID number {id} does not exist."));
+    }
+
+    /// <summary>
+    /// Adds a payment to an account
+    /// </summary>
+    /// <param name="payment"></param>
+    /// <returns></returns>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    public async Task<IActionResult> PostPayment(PaymentModel payment)
+    {
+      _logger.LogDebug("Adding a payment...");
+
+      await _unitOfWork.Payment.InsertAsync(payment);
+      await _unitOfWork.CommitAsync();
+
+      _logger.LogInformation($"Successfully added the payment {payment}.");
+
+      return Accepted(payment);
+
+    }
+
+    /// <summary>
+    /// Updates a payment
+    /// </summary>
+    /// <param name="payment"></param>
+    /// <returns></returns>
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> PutPayment(PaymentModel payment)
+    {
+      try
+      {
+        _logger.LogDebug("Updating a payment...");
+
+        _unitOfWork.Payment.Update(payment);
+        await _unitOfWork.CommitAsync();
+
+
+        _logger.LogInformation($"Successfully updated the payment {payment}.");
+
+        return Accepted(payment);
+      }
+
+      catch
+      {
+        _logger.LogWarning($"This payment does not exist.");
+
+        return NotFound(new ErrorObject($"Payment with ID number {payment.EntityId} does not exist"));
+      }
+    }
+    /// <summary>
+    /// Delete a user's profile
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteProfile(int id)
+    {
+      try
+      {
+        _logger.LogDebug("Deleting a profile by its ID number...");
+
+        await _unitOfWork.Profile.DeleteAsync(id);
+        await _unitOfWork.CommitAsync();
+
+        _logger.LogInformation($"Deleted the profile with ID number {id}.");
+
+        return Ok(MessageObject.Success);
+      }
+      catch
+      {
+        _logger.LogWarning($"Profile with ID number {id} does not exist.");
+
+        return NotFound(new ErrorObject($"Profile with ID number {id} does not exist."));
+      }
+    }
+
+    /// <summary>
+    /// Get all profiles
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<ProfileModel>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetProfile()
+    {
+      _logger.LogInformation($"Retrieved the profiles.");
+
+      return Ok(await _unitOfWork.Profile.SelectAsync());
+    }
+
+    /// <summary>
+    /// Get a user's profile with profile ID number
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(ProfileModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetProfile(int id)
+    {
+      ProfileModel profileModel;
+
+      _logger.LogDebug("Getting a profile by its ID number...");
+
+      profileModel = await _unitOfWork.Profile.SelectAsync(id);
+
+
+      if (profileModel is ProfileModel theProfile)
+      {
+        _logger.LogInformation($"Retrieved the profile with ID: {id}.");
+
+        return Ok(theProfile);
+      }
+
+      _logger.LogWarning($"Profile with ID number {id} does not exist.");
+
+      return NotFound(new ErrorObject($"Profile with ID number {id} does not exist."));
+    }
+
+    /// <summary>
+    /// Add a profile to an account
+    /// </summary>
+    /// <param name="profile"></param>
+    /// <returns></returns>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    public async Task<IActionResult> PostProfile(ProfileModel profile)
+    {
+      _logger.LogDebug("Adding a profile...");
+
+      await _unitOfWork.Profile.InsertAsync(profile);
+      await _unitOfWork.CommitAsync();
+
+      _logger.LogInformation($"Successfully added the profile {profile}.");
+
+      return Accepted(profile);
+    }
+
+    /// <summary>
+    /// Update a user's profile
+    /// </summary>
+    /// <param name="profile"></param>
+    /// <returns></returns>
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> PutProfile(ProfileModel profile)
+    {
+      try
+      {
+        _logger.LogDebug("Updating a profile...");
+
+        _unitOfWork.Profile.Update(profile);
+        await _unitOfWork.CommitAsync();
+
+        _logger.LogInformation($"Successfully updated the profile {profile}.");
+
+        return Accepted(profile);
+      }
+      catch
+      {
+        _logger.LogWarning($"This profile does not exist.");
+
+        return NotFound(new ErrorObject($"Profile with ID number {profile.EntityId} does not exist."));
+      }
+
+    }
   }
 }
